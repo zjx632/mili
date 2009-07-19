fd1fb2e1eb662c2a1aeef2ff139e09784a904f2a
danielgutson <danielgutson@localhost>
2009-07-19 20:33:00 +0000

    Version 9.2 ===========     * Added BasicType, and const/ref removals to type-mapper.

--- include/type_mapper.h
+++ include/type_mapper.h
@@ -42,6 +42,35 @@ type_mapper: A minimal library that implements a type mapper.
         typedef to_type type;                       \
     }
 
+#define add_mapping_const_removal(mapper)   \
+    template <class From>                   \
+    struct mapper<const From>               \
+    {                                       \
+        typedef From type;                  \
+    }
+
+#define add_mapping_ref_removal(mapper) \
+    template <class From>               \
+    struct mapper<From&>                \
+    {                                   \
+        typedef From type;              \
+    }
+
+#define add_mapping_const_ref_removal(mapper)   \
+    template <class From>                       \
+    struct mapper<const From&>                  \
+    {                                           \
+        typedef From type;                      \
+    }
+
 #define map_type(mapper, from_type) typename mapper<from_type>::type
+
+// Some useful mappers:
+
+// Removes const and &:
+declare_type_mapper(BasicType);
+add_mapping_const_removal(BasicType);
+add_mapping_ref_removal(BasicType);
+add_mapping_const_ref_removal(BasicType);
 
 #endif
