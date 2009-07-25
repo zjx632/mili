f589a86543f0b463fa1f016786aacf55b3362850
danielgutson <danielgutson@localhost>
2009-07-25 08:53:25 +0000

    Improved version of the type-mapper removals.

--- include/type_mapper.h
+++ include/type_mapper.h
@@ -42,25 +42,18 @@ type_mapper: A minimal library that implements a type mapper.
         typedef to_type type;                       \
     }
 
-#define add_mapping_const_removal(mapper)   \
-    template <class From>                   \
-    struct mapper<const From>               \
-    {                                       \
-        typedef From type;                  \
+#define add_mapping_const_removal(mapper)           \
+    template <class From>                           \
+    struct mapper<const From>                       \
+    {                                               \
+        typedef typename mapper<From>::type type;   \
     }
 
-#define add_mapping_ref_removal(mapper) \
-    template <class From>               \
-    struct mapper<From&>                \
-    {                                   \
-        typedef From type;              \
-    }
-
-#define add_mapping_const_ref_removal(mapper)   \
-    template <class From>                       \
-    struct mapper<const From&>                  \
-    {                                           \
-        typedef From type;                      \
+#define add_mapping_ref_removal(mapper)             \
+    template <class From>                           \
+    struct mapper<From&>                            \
+    {                                               \
+        typedef typename mapper<From>::type type;   \
     }
 
 #define map_type(mapper, from_type) typename mapper<from_type>::type
@@ -71,6 +64,5 @@ type_mapper: A minimal library that implements a type mapper.
 declare_type_mapper(BasicType);
 add_mapping_const_removal(BasicType);
 add_mapping_ref_removal(BasicType);
-add_mapping_const_ref_removal(BasicType);
 
 #endif
