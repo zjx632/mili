fec347b7722e97639c4b136fc4c3f557b7ad2b6e
matias tripod <matias.tripod@gmail.com>
2012-03-14 19:18:17 +0000

    Commit changes due to merge with branch issue60

--- tests/binary-streams_test.cpp
+++ tests/binary-streams_test.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2012 Lucas Paradisi, FuDePAN
+    Copyright (C) 2012 Lucas Paradisi, Matias Tripode, FuDePAN
 
     This file is part of the MiLi Minimalistic Library.
 
@@ -27,8 +27,9 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-#define BSTREAMS_DEBUG
 #include "mili/mili.h"
+
+
 
 using namespace mili;
 
@@ -47,15 +48,22 @@ void assertArrayEquals(const T& a1, const T& a2, unsigned int size)
 
 std::string get_TypeId(const std::string& s)
 {
-    size_t a;
+    uint32_t a;
 
-    s.copy(reinterpret_cast<char*>(&a), sizeof(size_t), 0);
+    s.copy(reinterpret_cast<char*>(&a), sizeof(uint32_t), 0);
 
-    return s.substr(sizeof(size_t), sizeof(char) * a);
+    if ((sizeof(uint32_t) + sizeof(char) * a) <= s.length()) //To avoid exception when non-debug info
+        return s.substr(sizeof(uint32_t), sizeof(char) * a);
+    else
+        return s;
 }
 
 struct A
 {
+
+    typedef class mili::bostream<mili::DebugPolicyBostream> bostream;
+    typedef class mili::bistream<mili::DebugPolicyBistream> bistream;
+
     unsigned int id;
     std::string name;
     long double a;
@@ -82,15 +90,50 @@ struct A
 
     bool operator==(const A& o) const
     {
-        bool result(false);
-        if (id == o.id && name == o.name && a == o.a)
-            result = true;
-        return result;
+        return  (id == o.id && name == o.name && a == o.a);
+    }
+};
+
+
+struct B
+{
+
+    typedef class mili::bostream<mili::NoDebugPolicyBostream> bostream;
+    typedef class mili::bistream<mili::NoDebugPolicyBistream> bistream;
+
+    unsigned int id;
+    std::string name;
+    long double b;
+
+    B()
+        : id(0), name(), b(0)
+    {}
+
+    B(unsigned int Id, const std::string& Name, long double B)
+        : id(Id), name(Name), b(B)
+    {}
+
+    friend bostream& operator<<(bostream& out, const B& o)
+    {
+        out << o.id << o.name << o.b;
+        return out;
+    }
+
+    friend bistream& operator>>(bistream& in, B& o)
+    {
+        in >> o.id >> o.name >> o.b;
+        return in;
+    }
+
+    bool operator==(const B& o) const
+    {
+        return (id == o.id && name == o.name && b == o.b);
     }
 };
 
 TEST(BinaryStream, BSTREAMS_DEBUG_identifier_test)
 {
+    typedef class mili::bostream<mili::DebugPolicyBostream> bostream;
     bostream bos;
 
     bos << -13;
@@ -122,8 +165,47 @@ TEST(BinaryStream, BSTREAMS_DEBUG_identifier_test)
     bos.clear();
 }
 
+TEST(BinaryStream, BSTREAMS_NO_DEBUG_identifier_test)
+{
+
+    typedef class mili::bostream<mili::NoDebugPolicyBostream> bostream;
+    bostream bos;
+
+    bos << -13;
+    ASSERT_NE(typeid(int).name(), get_TypeId(bos.str()));
+    bos.clear();
+
+    bos << 13u;
+    ASSERT_NE(typeid(unsigned int).name(), get_TypeId(bos.str()));
+    bos.clear();
+
+    bos << 1.2f;
+    ASSERT_NE(typeid(float).name(), get_TypeId(bos.str()));
+    bos.clear();
+
+    bos << 's';
+    ASSERT_NE(typeid(char).name(), get_TypeId(bos.str()));
+    bos.clear();
+
+    bos << double(1.22);
+    ASSERT_NE(typeid(double).name(), get_TypeId(bos.str()));
+    bos.clear();
+
+    bos << 1.22L;
+    ASSERT_NE(typeid(long double).name(), get_TypeId(bos.str()));
+    bos.clear();
+
+    bos << std::string("FuDePAN");
+    ASSERT_NE(typeid(uint32_t).name(), get_TypeId(bos.str()));
+    bos.clear();
+}
+
+
 TEST(BinaryStream, BSTREAMS_DEBUG_typemismatch_test)
 {
+    typedef class mili::bostream<mili::DebugPolicyBostream> bostream;
+    typedef class mili::bistream<mili::DebugPolicyBistream> bistream;
+
     bostream bos;
     const int int_loaded = 3;
     const char char_loaded[] = {'e', '4', 'f', 'r', 'y'};
@@ -140,10 +222,38 @@ TEST(BinaryStream, BSTREAMS_DEBUG_typemismatch_test)
     bis.str(bos.str());
     ASSERT_THROW(bis >> uchar_loaded, type_mismatch);
 }
+TEST(BinaryStream, BSTREAMS_NO_DEBUG_typemismatch_test)
+{
+    typedef class mili::bostream<mili::NoDebugPolicyBostream> bostream;
+    typedef class mili::bistream<mili::NoDebugPolicyBistream> bistream;
+
+    bostream bos;
+    const int int_loaded = 3;
+    const char char_loaded[] = {'e', '4', 'f', 'r', 'y'};
+
+    float float_loaded;
+    unsigned char uchar_loaded[5];
+
+    bos << int_loaded;
+    bistream bis(bos.str());
+
+
+    EXPECT_NO_THROW(bis >> float_loaded);
+
+    bos.clear();
+    bos << char_loaded;
+    bis.str(bos.str());
+    EXPECT_NO_THROW(bis >> uchar_loaded);
+}
 
 TEST(BinaryStream, chainedValues_test)
 {
+
+    typedef class mili::bostream<mili::DebugPolicyBostream> bostream;
+    typedef class mili::bistream<mili::DebugPolicyBistream> bistream;
+
     bostream bos;
+
     const float f_original = 1.2;
     const int i_original = 3;
     const double d_original = 0.89;
@@ -167,8 +277,90 @@ TEST(BinaryStream, chainedValues_test)
     ASSERT_EQ(b_original, b_loaded);
 }
 
+TEST(BinaryStream, chainedValues_No_Debug_test)
+{
+
+    typedef class mili::bostream<mili::NoDebugPolicyBostream> bostream;
+    typedef class mili::bistream<mili::NoDebugPolicyBistream> bistream;
+
+    bostream bos;
+
+    const float f_original = 1.2;
+    const int i_original = 3;
+    const double d_original = 0.89;
+    const B b_original(3, "pepe", 43);
+    const bool bool_original = true;
+
+    bos << f_original << i_original << d_original << b_original << bool_original;
+
+    bistream bis(bos.str());
+    float f_loaded;
+    int i_loaded;
+    double d_loaded;
+    B b_loaded;
+    bool bool_loaded;
+
+    bis >> f_loaded >> i_loaded >> d_loaded >> b_loaded >> bool_loaded;
+
+    ASSERT_EQ(f_original, f_loaded);
+    ASSERT_EQ(i_original, i_loaded);
+    ASSERT_EQ(d_original, d_loaded);
+    ASSERT_EQ(b_original, b_loaded);
+    ASSERT_EQ(bool_original, bool_loaded);
+
+}
+
 TEST(BinaryStream, contaniers_test)
 {
+
+    typedef class mili::bostream<mili::DebugPolicyBostream> bostream;
+    typedef class mili::bistream<mili::DebugPolicyBistream> bistream;
+
+    bostream bos;
+    std::vector<int> integers_original;
+    std::set<float> set_original;
+    std::list<char> list_original;
+    const double numbers_original[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    insert_into(integers_original, 6);
+    insert_into(integers_original, 7);
+    insert_into(integers_original, 8);
+    insert_into(integers_original, 9);
+    insert_into(integers_original, 0);
+    insert_into(integers_original, 1);
+    insert_into(set_original, 0.1f);
+    insert_into(set_original, 1.1f);
+    insert_into(set_original, 2.1f);
+    insert_into(set_original, 3.1f);
+    insert_into(set_original, 4.1f);
+    insert_into(list_original, 't');
+    insert_into(list_original, 'y');
+    insert_into(list_original, 'w');
+    insert_into(list_original, 'A');
+
+    bos << integers_original << set_original << list_original << numbers_original;
+
+    std::vector<int> integers_loaded;
+    std::set<float> set_loaded;
+    std::list<char> list_loaded;
+    double numbers_loaded[9];
+    bistream bis(bos.str());
+    bis >> integers_loaded >> set_loaded >> list_loaded >> numbers_loaded;
+
+    ASSERT_EQ(integers_original, integers_loaded);
+
+    ASSERT_EQ(set_original, set_loaded);
+
+    ASSERT_EQ(list_original, list_loaded);
+
+    assertArrayEquals(numbers_original, numbers_loaded, 9);
+}
+
+TEST(BinaryStream, No_Debug_Contaniers_Test)
+{
+    typedef class mili::bostream<mili::NoDebugPolicyBostream> bostream;
+    typedef class mili::bistream<mili::NoDebugPolicyBistream> bistream;
+
     bostream bos;
     std::vector<int> integers_original;
     std::set<float> set_original;
