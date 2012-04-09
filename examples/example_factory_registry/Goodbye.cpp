36b67043d4efff66e19003354023741c717f317c
brujofamaf <brujofamaf@gmail.com>
2012-04-09 20:57:26 +0000

    Added example of factory_registry.

---
+++ examples/example_factory_registry/Goodbye.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdio>
+#include"Speaker.h"
+#include"mili/mili.h"
+
+class Goodbye: public Speaker
+{
+    virtual void saySomething();
+};
+
+void Goodbye::saySomething()
+{
+    std::cout << "Goodbye" << std::endl;
+}
+
+REGISTER_FACTORIZABLE_CLASS(Speaker, Goodbye,std::string,std::string("Goodbye"));
