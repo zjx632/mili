bfd85bbbf774b61f9843be87e229e524cbfb583b
lbesso <lbesso@localhost>
2013-08-08 23:40:38 -0300

    [mili-89] Renamed files and fixed test cases

--- mili/safe_c_functions.h
+++ mili/safe_c_functions.h
@@ -1,13 +1,13 @@
 /*
-c_call_improved: improves some call to C functions like std::memcpy().
+safe_c_functions.h: improves some call to C functions like std::memcpy().
     This file is part of the MiLi Minimalistic Library.
 
     Copyright (C) Daniel Gutson,    FuDePAN 2013
                   Lucas Besso       FuDePAN 2013
     Distributed under the Boost Software License, Version 1.0.
-    (See accompanying file LICENSE_1_0.txt in the root directory or 
+    (See accompanying file LICENSE_1_0.txt in the root directory or
     copy at http://www.boost.org/LICENSE_1_0.txt)
-    
+
     MiLi IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
     FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
@@ -17,8 +17,8 @@ c_call_improved: improves some call to C functions like std::memcpy().
     DEALINGS IN THE SOFTWARE.
 */
 
-#ifndef C_CALL_IMPROVED_H
-#define C_CALL_IMPROVED_H
+#ifndef SAFE_C_FUNCTIONS_H
+#define SAFE_C_FUNCTIONS_H
 
 NAMESPACE_BEGIN
 
@@ -36,7 +36,6 @@ inline void safecpy(DEST* dest, const SRC* src, size_t count)
     _Validation::SRC_type_EqualTo_DEST(src);
     std::memcpy(dest, src, count);
 }
-
 
 NAMESPACE_END
 
