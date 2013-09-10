2540c1beb2e7afc8fb838609c28a156a46e7b6c4
lbesso <lbesso@localhost>
2013-09-09 23:34:13 -0300

    [issue-89] Update README and CHANGELOG for the issue 89. Also, the define 'SAFE_C_FUNCTIONS_H' was renamed to 'SAFE_C_FUNCTIONS'.

--- mili/mili.h
+++ mili/mili.h
@@ -145,7 +145,7 @@ MiLi: A set of minimal libraries composed only by 1 header file each.
 #   include "non_copyable.h"
 #endif
 
-#ifndef NO_SAFE_C_FUNCTIONS_H
+#ifndef NO_SAFE_C_FUNCTIONS
 #   include "safe_c_functions.h"
 #endif
 
