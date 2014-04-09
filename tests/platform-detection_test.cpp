d6b18d8b9381c7b3c8ee4bd7a3d57f73e87c2555
Pablo Oliva <pabloliva87@gmail.com>
2014-04-09 18:34:31 -0300

    [Issue110] Fixed broken tests and astyled code.

--- tests/platform-detection_test.cpp
+++ tests/platform-detection_test.cpp
@@ -3,9 +3,9 @@
 
     Copyright (C) Daniel Muñoz, FuDePAN 2012
     Distributed under the Boost Software License, Version 1.0.
-    (See accompanying file LICENSE_1_0.txt in the root directory or 
+    (See accompanying file LICENSE_1_0.txt in the root directory or
     copy at http://www.boost.org/LICENSE_1_0.txt)
-    
+
     MiLi IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
     FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
@@ -35,14 +35,18 @@ TEST(PlatformDetectionTest, OperatingSystem)
 
 TEST(PlatformDetectionTest, Compiler)
 {
-    ASSERT_EQ(MILI_COMPILER_VS,		1);
-    ASSERT_EQ(MILI_COMPILER_GCC,	2);
-    ASSERT_EQ(MILI_COMPILER_ICC,	3);
-    ASSERT_EQ(MILI_COMPILER_UNKNOWN,	4);
+    ASSERT_EQ(MILI_COMPILER_VS,		 1);
+    ASSERT_EQ(MILI_COMPILER_GCC,	 2);
+    ASSERT_EQ(MILI_COMPILER_ICC,	 3);
+    ASSERT_EQ(MILI_COMPILER_CLANG,   4);
+    ASSERT_EQ(MILI_COMPILER_TICCS,   5);
+    ASSERT_EQ(MILI_COMPILER_UNKNOWN, 6);
 
     ASSERT_TRUE( (MILI_COMPILER == MILI_COMPILER_VS)	\
                ^ (MILI_COMPILER == MILI_COMPILER_GCC)	\
                ^ (MILI_COMPILER == MILI_COMPILER_ICC)	\
+               ^ (MILI_COMPILER == MILI_COMPILER_CLANG) \
+               ^ (MILI_COMPILER == MILI_COMPILER_TICCS) \
                ^ (MILI_COMPILER == MILI_COMPILER_UNKNOWN) );
 }
 
