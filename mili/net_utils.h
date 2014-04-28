fa9afcc14a0f0d80004662433a7f6f3bef67028a
Emanuel Bringas <Emanuel Bringas@localhost>
2014-04-28 11:15:19 -0300

    Added hton/ntoh functions for int64/uint64

--- mili/net_utils.h
+++ mili/net_utils.h
@@ -21,44 +21,92 @@
 
 NAMESPACE_BEGIN
 
-inline int32_t hton(int32_t value)
+union int64Handler
 {
-    return htonl(value);
+    int64_t u64;
+    struct
+    {
+        int32_t high32;
+        int32_t low32;
+    };
+};
+
+inline int16_t hton(const int16_t value)
+{
+    return static_cast<int16_t>(htons(static_cast<uint16_t>(value)));
 }
 
-inline int16_t hton(int16_t value)
+inline uint16_t hton(const uint16_t value)
 {
     return htons(value);
 }
 
-inline int32_t ntoh(int32_t value)
+inline int32_t hton(const int32_t value)
 {
-    return ntohl(value);
+    return static_cast<int32_t>(htonl(static_cast<uint32_t>(value)));
 }
 
-inline int16_t ntoh(int16_t value)
-{
-    return ntohs(value);
-}
-
-inline uint32_t hton(uint32_t value)
+inline uint32_t hton(const uint32_t value)
 {
     return htonl(value);
 }
 
-inline uint16_t hton(uint16_t value)
+inline int64_t hton(const int64_t value)
 {
-    return htons(value);
+    int64Handler input;
+    input.u64 = value;
+
+    input.high32 = hton(input.high32);
+    input.low32 = hton(input.low32);
+
+    std::swap(input.high32, input.low32);
+
+    return input.u64;
+}
+   
+inline uint64_t hton(const uint64_t value)
+{
+    return static_cast<uint64_t>(hton(static_cast<int64_t>(value)));
 }
 
-inline uint32_t ntoh(uint32_t value)
+/** Network to host **/
+
+inline int16_t ntoh(const int16_t value)
+{
+    return static_cast<int16_t>(ntohs(static_cast<uint16_t>(value)));
+}
+
+inline uint16_t ntoh(const uint16_t value)
+{
+    return ntohs(value);
+}
+
+inline int32_t ntoh(const int32_t value)
+{
+    return static_cast<int32_t>(ntohl(static_cast<uint32_t>(value)));
+}
+
+inline uint32_t ntoh(const uint32_t value)
 {
     return ntohl(value);
 }
 
-inline uint16_t ntoh(uint16_t value)
+inline int64_t ntoh(const int64_t value)
 {
-    return ntohs(value);
+    int64Handler input;
+    input.u64 = value;
+
+    input.high32 = ntoh(input.high32);
+    input.low32 = ntoh(input.low32);
+
+    std::swap(input.high32, input.low32);
+    
+    return input.u64;
+}
+
+inline uint64_t ntoh(const uint64_t value)
+{
+    return static_cast<uint64_t>(ntoh(static_cast<int64_t>(value)));
 }
 
 NAMESPACE_END
